c08fc0a3eb0bdfb18f7232634ba0bbc801c188bc
Filip Tsolov <filip.tsolov@code.berlin>
2021-04-09 20:33:47 +0200

    added logo & more landing styling

--- main/website.py
+++ main/website.py
@@ -8,5 +8,14 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    return render_template(login.html)
+
+
+# @app.route('/log')
+
+# @app.route('/overview')
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
