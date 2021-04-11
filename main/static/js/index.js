b30b106e21db33d5fa10bfbd5cfdd26b849dceca
Filip Tsolov <filip.tsolov@code.berlin>
2021-04-11 16:09:44 +0200

    refactored password security js

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -1,27 +1,23 @@
-function is_numeric(str){
-    return /^\d+$/.test(str);
+let passwordField = document.querySelector('#password');
+let passwordRepeatedField = document.querySelector('#password-repeat');
+
+document.getElementById('form').addEventListener('submit', event => {
+    let password = passwordField.value;
+    let passwordRepeated = passwordRepeatedField.value;
+
+    if (!checkPasswordSecurity(password)) {
+        event.preventDefault();
+        alert("Password not valid.")
+        return
+    }
+
+    if (password !== passwordRepeated) {
+        event.preventDefault();
+        alert("Passwords don't match.")
+    }
+})
+
+function checkPasswordSecurity(password) {
+    return password.length >= 8 && /\d/.test(password)
 }
 
-function password_check() {
-    let password = document.getElementById("#password").value;
-    console.log(password)
-    if (password.length > 6) {
-        let i;
-        for (i=0; i < password.length; i++) {
-            let bool = is_numeric(letter);
-            if (bool === true) {
-                return true;
-            }
-        }
-    }
-}
-
-function password_repeat_check() {
-    let password = document.querySelector('#password').value;
-    let password_repeated = document.querySelector('#password-repeat').value;
-    if (password === password_repeated) {
-        console.log("true")
-    } else {
-        console.log("false")
-    }
-}
