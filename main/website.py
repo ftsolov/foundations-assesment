b99d9d37cb17feecb21c21eb6a3a5d5d0ff52af6
Filip Tsolov <filip.tsolov@code.berlin>
2021-04-11 11:34:20 +0200

    added dashboard view

--- main/website.py
+++ main/website.py
@@ -18,9 +18,10 @@ def login():
     return render_template('login.html')
 
 
-# @app.route('/log')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
-# @app.route('/overview')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
