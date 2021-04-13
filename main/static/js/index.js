878d3505e3f5ce2761619da0694ef1ee0f7842ea
Filip Tsolov <filip.tsolov@code.berlin>
2021-04-13 15:03:47 +0200

    added new flask app routes (+pseudo code for them) and created new daily entry function

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -5,17 +5,19 @@ document.getElementById("form").addEventListener("submit", (event) => {
   let password = passwordField.value;
   let passwordRepeated = passwordRepeatedField.value;
 
-  if (!checkPasswordSecurity(password)) { // checks if password meets criteria
-	event.preventDefault();
-	alert("Password does not meet criteria.");
-	resetPasswordInputs();
-	return;
+  if (!checkPasswordSecurity(password)) {
+    // checks if password meets criteria
+    event.preventDefault();
+    alert("Password does not meet criteria.");
+    resetPasswordInputs();
+    return;
   }
 
-  if (password !== passwordRepeated) { // checks if password is repeated properly
-	event.preventDefault();
-	alert("Passwords don't match, please try again.");
-	resetPasswordInputs();
+  if (password !== passwordRepeated) {
+    // checks if password is repeated properly
+    event.preventDefault();
+    alert("Passwords don't match, please try again.");
+    resetPasswordInputs();
   }
 });
 
@@ -23,16 +25,27 @@ function checkPasswordSecurity(password) {
   return password.length >= 8 && /\d/.test(password);
 }
 
-function resetPasswordInputs() { // resets input field values
+function resetPasswordInputs() {
+  // resets input field values
   passwordField.value = "";
   passwordRepeatedField.value = "";
 }
 
-function logNewEntry() {
-    let popup = document.getElementById('new-entry')
-    if (popup.style.display === "none") {
-    popup.style.display = "block";
-  } else {
-    popup.style.display = "none";
+function logNewEntryAnimation() {
+  let popup = document.getElementById("popup");
+  if (popup.style.display === "block") {
+    popup.style.opacity = "1";
+    popup.style.zIndex = "10";
+  } else if (popup.style.display === "none") {
+    popup.style.opacity = "1";
+    popup.style.zIndex = "10";
+  }
+}
+
+function closeNewEntryOverlay() {
+  let popup = document.getElementById("popup");
+  if (popup.style.display === "block") {
+    popup.style.opacity = "0";
+    popup.style.zIndex = "-10";
   }
 }
--- main/website.py
+++ main/website.py
@@ -11,19 +11,51 @@ def index():
     return render_template('landing.html')
 
 
-@app.route('/sign-up')
-def signup():
+@app.route('/sign_up_page')
+def signup_page():
+    # display the login page
     return render_template('signup.html')
 
 
-@app.route('/login')
-def login():
+@app.route('/sign_up')
+def signup():
+    # check if password meets criteria (if not, prompt error)
+    # check if password repeat matches (if not, prompt error)
+    # read post body
+    # check if inputs are both filled in and not blank
+    # check if username exists in the database
+    # if it exists, prompt error
+    # if it doesnt and everything is good, create a new user entry in the database
+    # return dashboard of the user
+    return render_template('signup.html')
+
+
+@app.route('/login_page')
+def login_page():
+    # display the login page
+    return render_template('login.html')
+
+
+@app.route('/login_check')
+def login_check():
+    # read request body
+    # check if inputs are both filled in and not blank
+    # check if username exists in the database
+    # get password from body request and hash it
+    # compare hashed client side password with user password on server
+    # if correct, display dashboard
+    # if wrong, prompt again and display error message
     return render_template('login.html')
 
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+
+@app.route('/fetch_daily_entries')
+def fetch_daily_entries():
+    pass
 
 
 if __name__ == "__main__":
