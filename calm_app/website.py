4314d787590926b83d941659e9f68eb1f07ee6c4
Filip Tsolov <filip.tsolov@code.berlin>
2021-04-24 19:38:18 +0200

    another attempt

--- calm_app/website.py
+++ calm_app/website.py
@@ -12,7 +12,7 @@ app.secret_key = "safari"
 app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:database123@localhost/calmdatabase'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{os.environ.get("DB_PASSWORD")}@34.89.139.25:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mAliH3amCvivPG9E@34.89.139.25:5432/postgres'
 app.debug = True
 app.permanent_session_lifetime = timedelta(days=1)
 db = SQLAlchemy(app)
